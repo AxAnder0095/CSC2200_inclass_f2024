9f750e6ad886b48f61ce4524bc3d6bc70e0a1256
Frenguin <104327737+Frenguin@users.noreply.github.com>
2024-11-08 14:17:25 -0600

    updated the game

--- Classwork/js/game.js
+++ Classwork/js/game.js
@@ -1,44 +1,87 @@
 const gameBoard = {
     size: 4,
     board: [],
+    moves: 0,
+    score: 0,
     icons: {
-        player: '<i className="fa-solid fa-chess-knight"></i>',
-        obstacle: '<i className="fa-solid fa-dungeon"></i>',
-        collectable: `<i class="fa-solid fa-ring"></i>`
+        player: '<i class="fa-solid fa-chess-knight player-icon"></i>',
+        obstacle: '<i class="fa-solid fa-dungeon obstacle-icon"></i>',
+        collectible: '<i class="fa-solid fa-ring collectible-icon"></i>'
     },
-    odds: {
-        chanceOfBlock: .2,
-        chanceOfCollectable: .3
+    playerPosition: { row: 0, col: 0 },
+    odds : {
+        chanceOfBlock : .2,
+        chanceOfCollect : .3
     },
     obstacles: [],
-    collectables: [],
-    init() { // method used to initialize the game
+    collectibles: [],
+    init() {
+        // alert("The game is a foot");
         this.createEmptyBoard();
-        this.setUpBoard();
-    },
-    createEmptyBoard() {
-        for (let i = 0; i < this.size; i++){
-            let row = Array(this.size).fill('');
-            this.board.push(row);
-        }
+        this.setUpBoard()
+        this.render();
     },
     setUpBoard() {
-        for (let row = 0; row < this.size; row++) {
-            for (let col = 0; col < this.size; col++) {
-                if (Math.random() < this.odds.chanceOfBlock) {
-                    this.board[row][col] = '0';
-                    this.obstacles.push({row: row, col: col});
-                }
-                else if (Math.random() < this.odds.chanceOfCollectable) {
-                    this.board[row][col] = 'c';
-                    this.collectables.push({row: row, col: col});
+        for( let row = 0; row < this.size; row++ ) {
+            for( let col = 0; col < this.size; col++ ) {
+                if ( Math.random() < this.odds.chanceOfBlock) {
+                    this.board[row][col] = 'O';
+                    this.obstacles.push({ row: row, col: col });
+                } else if (Math.random() <this.odds.chanceOfCollect) {
+                    this.board[row][col] = 'C';
+                    this.collectibles.push({ row: row, col: col });
                 }
             }
         }
+        this.board[this.playerPosition.row][this.playerPosition.col] = 'P';
+    },
+    move(direction) {
+        alert("inside move direction" + direction);
+    },
+    createEmptyBoard() {
+        for( let i = 0; i < this.size; i++ ) {
+            let row = Array(this.size).fill('');
+            this.board.push(row);  // push appends a row on to the array
+        }
+    },
+    render(){
+        const gameBoardDOM = document.getElementById('game-board');
+        gameBoardDOM.innerHTML = '';  // clear it out
+        for( let row = 0; row < this.size; row++ ) {
+            for( let col = 0; col < this.size; col++ ) {
+                const tile = document.createElement('div');
+                tile.className = 'tile'; // <div class='tile'
+                if ( this.board[row][col] === 'P' ) {
+                    tile.innerHTML = this.icons.player;
+                } else if ( this.board[row][col] === 'O' ) {
+                    tile.innerHTML = this.icons.obstacle;
+                } else if (this.board[row][col] === 'C') {
+                    tile.innerHTML = this.icons.collectible;
+                }
+                gameBoardDOM.appendChild( tile);
+            }
+        }
+        document.getElementById('move-count').textContent =
+            `Moves: ${this.moves}`;
     }
 }
-
 document.addEventListener("DOMContentLoaded", function() {
     gameBoard.init();
-    alert(`board: ${this.board.length}`);
+    // alert(`Board:${ gameBoard.board.length }`);
+    window.addEventListener('keydown', (e) => {
+        switch (e.key) {
+            case 'ArrowUp':
+                gameBoard.move('up');
+                break;
+            case 'ArrowDown':
+                gameBoard.move('down');
+                break;
+            case 'ArrowLeft':
+                gameBoard.move('left');
+                break;
+            case 'ArrowRight':
+                gameBoard.move('right');
+                break;
+        }
+    })
 })
